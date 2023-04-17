373c6c027a66cf8225f1322a30e80f2fe4cb9335
IceK37 <45316097+IceK37@users.noreply.github.com>
2023-04-17 15:14:24 +0200

    table notation

--- src/main/java/com/example/projetjee/model/notation.java
+++ src/main/java/com/example/projetjee/model/notation.java
@@ -1,11 +1,46 @@
 package com.example.projetjee.model;
 
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+
 @Entity
 @Table(name = "notation")
 public class notation {
 
+    @Column(name = "note")
+    private int name;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    Long userID;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "programme_id")
+    Long programmmeId;
+
+    public int getName() {
+        return name;
+    }
+
+    public void setName(int name) {
+        this.name = name;
+    }
+
+    public Long getUserID() {
+        return userID;
+    }
+
+    public void setUserID(Long userID) {
+        this.userID = userID;
+    }
+
+    public Long getProgrammmeId() {
+        return programmmeId;
+    }
+
+    public void setProgrammmeId(Long programmmeId) {
+        this.programmmeId = programmmeId;
+    }
 }
--- src/main/java/com/example/projetjee/model/programme.java
+++ src/main/java/com/example/projetjee/model/programme.java
@@ -9,7 +9,11 @@ public class programme {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    public Long getId() {
+        return id;
+    }
 
-
-
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
